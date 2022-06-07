e554b77548609b26f49df9c56240533e5b59be04
Iris <33935289+Irhystea@users.noreply.github.com>
2022-06-07 14:44:51 +0200

    Toggle is succesful

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuButton = document.getElementById('toggle-button');
+let menuItems = menuButton.nextElementSibling;
+
+menuButton.addEventListener('click',function(){menuItems.classList.toggle('menu-display')})
+
+// ;
