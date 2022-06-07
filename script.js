5b7e35ba79ec27c85fed6cef129a8fbdfecb93dd
Iris <33935289+Irhystea@users.noreply.github.com>
2022-06-07 16:25:08 +0200

    Almost
    Everything works except for the ability to switch between colours.

--- script.js
+++ script.js
@@ -7,39 +7,51 @@ let orangeButton = menuItems.children[2];
 let greenButton = menuItems.children[3];
 let purpleButton = menuItems.children[4];
 
-menuButton.addEventListener('click',function(){menuItems.classList.toggle('menu-display')});
+menuButton.addEventListener('click',function(){
+    menuItems.classList.toggle('menu-display');
+    return;
+});
 
-
+homeButton.classList.add('home-background');
+redButton.classList.add('red-background');
+orangeButton.classList.add('orange-background');
+greenButton.classList.add('green-background');
+purpleButton.classList.add('purple-background');
 
 //Button Colours
 homeButton.addEventListener('click', function(){
     console.log("Background Gray")
     document.body.classList.add('home-background');
+    menuItems.classList.toggle('menu-display');
+    return;
 });
 
 redButton.addEventListener('click', function(){
     console.log("Background Red");
     document.body.classList.add('red-background');
+    menuItems.classList.toggle('menu-display');
+    return;
 });
 
 orangeButton.addEventListener('click', function(){
     console.log("Background Orange")
     document.body.classList.add('orange-background');
+    menuItems.classList.toggle('menu-display');
+    return;
 });
 
 greenButton.addEventListener('click', function(){
     console.log("Background Green")
     document.body.classList.add('green-background');
+    menuItems.classList.toggle('menu-display');
+    return;
 });
 
 purpleButton.addEventListener('click', function(){
     console.log("Background Purple")
     document.body.classList.add('purple-background');
+    menuItems.classList.toggle('menu-display');
+    return;
 })
-
-// ;
-// 
-// 
-// 
 
 
