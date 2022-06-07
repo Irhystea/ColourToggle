04a2a22b211cf37e3b0e8abcc98b4ba5d1673cd9
Iris <33935289+Irhystea@users.noreply.github.com>
2022-06-07 14:27:15 +0200

    ok

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let navigation = document.getElementById("menuToggle");
-
-// let navigationButton = 
-
-
-//when the button gets pressed, the menu toggles between opened and closed;
+document.getElementById('menu-image')
