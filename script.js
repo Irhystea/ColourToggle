8267a20983be3689bc096fc8a722bb7295efc195
Iris <33935289+Irhystea@users.noreply.github.com>
2022-06-14 13:14:02 +0200

    sd

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-let menuButton = document.getElementById('toggle-button');
-
-let menuItems = menuButton.nextElementSibling;
-let homeButton = menuItems.children[0];
-let redButton = menuItems.children[1];
-let orangeButton = menuItems.children[2];
-let greenButton = menuItems.children[3];
-let purpleButton = menuItems.children[4];
+const menuButton = document.getElementById('toggle-button');
+const menuItems = menuButton.nextElementSibling;
 
 menuButton.addEventListener('mouseover',function(){
+    console.log("Mouse hovered")
     menuItems.classList.toggle('menu-display');
     return;
 });
 
+console.log(menuItems)
 
-homeButton.classList.add('home-background');
-redButton.classList.add('red-background');
-orangeButton.classList.add('orange-background');
-greenButton.classList.add('green-background');
-purpleButton.classList.add('purple-background');
 
-//Button Colours
+// const homeButton = menuItems.children[0];
+// const redButton = menuItems.children[1];
+// const orangeButton = menuItems.children[2];
+// const greenButton = menuItems.children[3];
+// const purpleButton = menuItems.children[4];
 
-homeButton.addEventListener('click', function(){
-    console.log("Background Gray")
-    document.body.classList.add('home-background');
-    document.body.classList.remove('red-background', 'orange-background', 'green-background', 'purple-background');
 
-    menuItems.classList.toggle('menu-display');
-    return;
-});
 
-redButton.addEventListener('click', function(){
-    console.log("Background Red");
-    document.body.classList.add('red-background');
-    document.body.classList.remove('orange-background', 'green-background', 'purple-background');
+// //Button Colours
 
-    menuItems.classList.toggle('menu-display');
-    return;
-});
+// homeButton.addEventListener('click', function(){
+//     console.log("Background Gray")
+//     document.body.classList.add('home-background');
+//     document.body.classList.remove('red-background', 'orange-background', 'green-background', 'purple-background');
 
-orangeButton.addEventListener('click', function(){
-    console.log("Background Orange")
-    document.body.classList.add('orange-background');
-    document.body.classList.remove('green-background', 'purple-background');
+//     menuItems.classList.toggle('menu-display');
+//     return;
+// });
 
-    menuItems.classList.toggle('menu-display');
-    return;
-});
+// redButton.addEventListener('click', function(){
+//     console.log("Background Red");
+//     document.body.classList.add('red-background');
+//     document.body.classList.remove('orange-background', 'green-background', 'purple-background');
 
-greenButton.addEventListener('click', function(){
-    console.log("Background Green")
-    document.body.classList.add('green-background');
-    document.body.classList.remove('purple-background');
+//     menuItems.classList.toggle('menu-display');
+//     return;
+// });
 
-    menuItems.classList.toggle('menu-display');
-    return;
-});
+// orangeButton.addEventListener('click', function(){
+//     console.log("Background Orange")
+//     document.body.classList.add('orange-background');
+//     document.body.classList.remove('green-background', 'purple-background');
 
-purpleButton.addEventListener('click', function(){
-    console.log("Background Purple")
-    document.body.classList.add('purple-background');
+//     menuItems.classList.toggle('menu-display');
+//     return;
+// });
 
-    menuItems.classList.toggle('menu-display');
-    return;
-})
+// greenButton.addEventListener('click', function(){
+//     console.log("Background Green")
+//     document.body.classList.add('green-background');
+//     document.body.classList.remove('purple-background');
+
+//     menuItems.classList.toggle('menu-display');
+//     return;
+// });
+
+// purpleButton.addEventListener('click', function(){
+//     console.log("Background Purple")
+//     document.body.classList.add('purple-background');
+
+//     menuItems.classList.toggle('menu-display');
+//     return;
+// });
+
+
 
 
